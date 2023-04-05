0c5ac68a580d60cd0793aa619b4ce12ecb237406
Pranay Singhania <pranay.s4299@gmail.com>
2023-04-05 23:56:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+// };
+// function greet() {
+//   return `${this.firstName}`;
+// }
+// let result = greet(person);
+
+// console.log(result);
+
+// const getData = () => {
+//   console.log("Pressed");
+// };
+
+// Debounce
+
+// const debounce = (fn, delay) => {
+//   let timer;
+//   return (...args) => {
+//     let context = this;
+//     clearTimeout(timer);
+//     timer = setTimeout(() => {
+//       fn.apply(context, args);
+//     }, delay);
+//   };
+// };
+
+// Throttle
+
+// const throttle = (fn, delay) => {
+//   let flag = true;
+//   return (...args) => {
+//     let context = this;
+//     if (flag) {
+//       fn.apply(context, args);
+//       flag = false;
+//       setTimeout(() => {
+//         flag = true;
+//       }, delay);
+//     }
+//   };
+// };
+
+// const betterFunction = debounce(getData, 700);
+// const betterFunction = throttle(getData, 2000);
